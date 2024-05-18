b3252abcbf84248473ce62917b12f9249d676ba1
FreeCodejunction <ahnafmottaki2022@gmail.com>
2024-05-18 22:41:10 +0600

    Finished the js project

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const template = document.querySelector(".pet-card-template");
+const wrapper = document.createDocumentFragment();
+
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
   const weatherData = await weatherPromise.json();
@@ -7,6 +11,44 @@ async function start() {
 }
 
 start();
+petFilter();
+
+async function petFilter() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+  const petsData = await petsPromise.json();
+  console.log(petsData);
+  petsData.forEach((pet, index) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-about").textContent = pet.description;
+    if (!pet.photo) {
+      pet.photo = "Fallback.jpg"
+    };
+    clone.querySelector(".pet-img img").src = pet.photo;
+    clone.querySelector(".pet-img img").alt = `a ${pet.species} named ${pet.name}`;
+    clone.querySelector(".pet-age").textContent = createPetAgeText(pet.bithYear);
+    clone.querySelector(".pet-grids").dataset.species = pet.species;
+
+    wrapper.appendChild(clone);
+  });
+
+
+
+  document.querySelector(".pet-area").appendChild(wrapper);
+
+}
+
+function createPetAgeText(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const petAge = currentYear - birthYear;
+  if (petAge < 1) {
+    return "less than a year old";
+  } else if (petAge === 1) {
+    return "1 year old"
+  } else if (petAge > 1) {
+    return `${petAge} years old`
+  }
+}
 
 
 const btn = document.querySelectorAll(".menu-bar button");
@@ -22,6 +64,12 @@ function addingActive(e) {
   e.target.classList.add("active");
 
   const currentFilter = e.target.dataset.filter;
-  console.log(currentFilter);
+  document.querySelectorAll(".pet-grids").forEach(pet => {
+    if (currentFilter === pet.dataset.species || currentFilter === "all") {
+      pet.style.display = "grid";
+    } else {
+      pet.style.display = "none";
+    }
+  })
 
 }
